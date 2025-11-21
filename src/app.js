64ea981ed85c7b583a7fb2e1d369dd6aec381289
Komal Joshi <moudgillkomal6@gmail.com>
2025-11-21 14:37:34 +0530

    modified:   src/app.js

--- src/app.js
+++ src/app.js
@@ -28,8 +28,11 @@ connectDB();
 
 // MIDDLEWARE
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://spendwise-chi.vercel.app"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
